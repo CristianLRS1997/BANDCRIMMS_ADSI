78deb491e47b3e97f799718721061948b543afc8
Dayana Marcela Cogaria Boada <dayanacogaria@gmail.com>
2017-07-31 20:43:58 -0500

    plantilla lista

---
+++ Vista/index.php
@@ -0,0 +1,39 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<head>
+    <title>Visitors an Admin Panel Category Bootstrap Responsive Website Template | Home :: w3layouts</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+
+
+    <?php include ("includes/imports.php"); ?>
+
+</head>
+<body>
+<section id="container">
+    <!--header start-->
+    <?php include ("includes/header.php"); ?>
+    <!--header end-->
+    <!--sidebar start-->
+    <?php include ("includes/menu.php"); ?>
+    <!--sidebar end-->
+    <!--main content start-->
+    <section id="main-content">
+        <?php include ("includes/main.php"); ?>
+        <!-- footer -->
+        <?php include ("includes/footer.php"); ?>
+        <!-- / footer -->
+    </section>
+    <!--main content end-->
+</section>
+<?php include ("includes/scripts.php"); ?>
+<!-- //calendar -->
+</body>
+</html>
